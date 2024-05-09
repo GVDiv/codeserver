a6093d7cd98fbd4b95296f54a2b5fe88f3c1da86
Gabriel Villamayor <gabitokun@outlook.com>
2024-05-09 02:53:04 -0300

    fix details and cart add styles

--- public/scripts/detailsProduct.js
+++ public/scripts/detailsProduct.js
@@ -8,8 +8,10 @@ fetch(`/api/products/${uid}`)
         const product = res.response
         document.querySelector("#detailsProduct").innerHTML=
         `
+        <div class="img_container">
+          <img src="${product.photo}" class="img_info" alt="...">
+        </div>
         <div class="card" style="width: 18rem;">
-            <img src="${product.photo}" class="card-img-top" alt="...">
             <div class="card-body">
                 <p class="info-text">title: ${product.title}</p>
                 <p class="info-text">category: ${product.category_id.category}</p>
@@ -17,9 +19,16 @@ fetch(`/api/products/${uid}`)
                 <p class="info-text">size: ${product.size_id.size}</p>
                 <p class="info-text">description: ${product.size_id.description}</p>
                 <p class="info-text">stock: ${product.stock}</p>
-                <button href="#" class="btn btn-primary" onclick="addToCart('${product._id}')">
-                  <i class="fa-solid fa-cart-plus"></i>
-                </button>
+                <div class="info_detail">
+                  <button href="#" class="btn btn-primary" onclick="addToCart('${product._id}')">
+                    <i class="fa-solid fa-cart-plus"></i>
+                    </button>
+                    <button class="btn btn-primary" type="button">
+                      <i class="fa-solid fa-plus"></i>
+                  </button>
+                 <input type="number" class="form-control" id="quantityInput" min="0" value="0" > 
+                </div>
+                      
             </div>
         </div>
         `
@@ -42,7 +51,32 @@ fetch(`/api/products/${uid}`)
           let response = await fetch(url, opts);
           response = await response.json();
           console.log(response);
+          location.reload()
         } catch (error) {
           console.log(error);
         }
       }
+
+  //CART COUNTER
+  fetch("/api/carts?user_id=6623ec94d8ef27548f40e5a3")
+  .then((res) => res.json())
+  .then((res) => {
+    const products = res.response;
+    console.log(products);
+    const cartElement = document.getElementById("cart");
+
+    if (products.length > 0) {
+      cartElement.classList.remove("cart-empty");
+      cartElement.classList.add("cart-with-products");
+      
+    } else {
+      cartElement.classList.remove("cart-with-products");
+      cartElement.classList.add("cart-empty");
+    }
+    const totalProducts = products.reduce(
+      (total, product) => total + product.quantity,
+      0
+    );
+    document.getElementById("cartTotal").textContent = `${totalProducts}`;
+  })
+  .catch((err) => console.log(err));
